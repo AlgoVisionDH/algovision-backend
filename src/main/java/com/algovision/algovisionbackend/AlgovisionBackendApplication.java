9f0ff139ecc072be7301dc5e9d58ce80a6c5a65b
st120712 <zzw123@naver.com>
2025-09-26 20:27:11 +0900

    first commit

---
+++ src/main/java/com/algovision/algovisionbackend/AlgovisionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.algovision.algovisionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgovisionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgovisionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/algovision/algovisionbackend/AlgovisionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algovision.algovisionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgovisionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
